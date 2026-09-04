78260bf3dbb0a736d3a15a39afb92fe615da5d04
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2026-09-04 13:07:30 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "s3_state_bucket_name" {
+  description = "The bucket created for state storage"
+  value       = module.state_backend.bucket_id
+}
+
+output "ssm_parameter_name" {
+  description = "SSM Parameter path storing the bucket name"
+  value       = aws_ssm_parameter.state_bucket_name.name
+}
