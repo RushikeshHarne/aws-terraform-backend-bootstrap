f105044686e04dbb1b08ee8ca9d91e1494b35026
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2026-09-04 13:57:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,17 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
 # 1. Call Reusable Remote Backend Module
 
 module "state_backend" {
   source      = "./Modules/remote_backend"
-  bucket_name = var.state_bucket_name
+  bucket_name = "${var.state_bucket_name}-${var.environment}-${random_string.suffix.result}"
 }
 
 # 2. Store Bucket Name in SSM Parameter Store for Repository 2 to fetch automatically
