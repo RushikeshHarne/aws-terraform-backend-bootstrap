06a859040074d81010bc3f4266fb887107f808e7
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2026-09-04 19:09:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,9 +24,9 @@ module "state_backend" {
 
 # 2. Store Bucket Name in SSM Parameter Store for Repository 2 to fetch automatically
 
-#resource "aws_ssm_parameter" "state_bucket_name" {
-#  name        = "/terraform/remote_state_bucket"
-# type        = "String"
- # value       = module.state_backend.bucket_id
-#  description = "S3 bucket name used for remote state storage"
-#}
+resource "aws_ssm_parameter" "state_bucket_name" {
+  name        = "/terraform/remote_state_bucket"
+  type        = "String"
+  value       = module.state_backend.bucket_id
+  description = "S3 bucket name used for remote state storage"
+}
